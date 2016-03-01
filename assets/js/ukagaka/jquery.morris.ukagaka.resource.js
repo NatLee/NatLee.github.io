670961aa876b7a1be946108e8fef6fc3735c2cbb
Nat <vip3268@gmail.com>
2016-03-02 00:16:45 +0800

    Update

--- assets/js/ukagaka/jquery.morris.ukagaka.resource.js
+++ assets/js/ukagaka/jquery.morris.ukagaka.resource.js
@@ -346,8 +346,6 @@
 		function get_ip_country()  {  
 			if (!empty($_SERVER['HTTP_CLIENT_IP'])) {  
 				$myip = $_SERVER['HTTP_CLIENT_IP'];  
-			} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
-				$myip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
 			} else {  
 			$myip = $_SERVER['REMOTE_ADDR'];  
 			}  
