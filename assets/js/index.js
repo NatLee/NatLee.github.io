3e3f7cd00331a9e8f71e8bb8423316a2fe778603
NatLee <natlee.work@gmail.com>
2023-04-28 23:25:29 +0800

    [update] rm dummy & add func

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,32 +2,39 @@ $(document).ready(function () {
     $.getJSON("assets/data/site.json", function (data) {
       let main = $("#main");
   
+      function createHeader(headerText) {
+        return $('<div class="header"></div>').html(`<h1 style="text-align: center">${headerText}</h1>`);
+      }
+  
+      function createArticle(article) {
+        return $(`
+          <article class="preview">
+            <span class="image">
+              <img src="${article.imgSrc}" alt="" />
+            </span>
+            <a target="_blank" rel="noopener noreferrer" href="${article.link}">
+              <h2>${article.title}</h2>
+              <div class="content">
+                <p>${article.description}</p>
+              </div>
+            </a>
+          </article>
+        `);
+      }
+  
       data.forEach((section) => {
-        let header = $('<div class="header"></div>').html(`<h1 style="text-align: center">${section.header}</h1>`);
-        main.append(header);
+        main.append(createHeader(section.header));
   
         let inner = $('<div class="inner"></div>');
         let tiles = $('<section class="tiles"></section>');
         inner.append(tiles);
   
         section.articles.forEach((article) => {
-          let preview = $(`
-            <article class="preview">
-              <span class="image">
-                <img src="${article.imgSrc}" alt="" />
-              </span>
-              <a target="_blank" rel="noopener noreferrer" href="${article.link}">
-                <h2>${article.title}</h2>
-                <div class="content">
-                  <p>${article.description}</p>
-                </div>
-              </a>
-            </article>
-          `);
-          tiles.append(preview);
+          tiles.append(createArticle(article));
         });
   
         main.append(inner);
+        main.append('<br>'); // Add a line break between sections
       });
     });
   });
