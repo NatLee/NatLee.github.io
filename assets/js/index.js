0cacade8256c2cd9b329752c6c4968b3c713f6b7
NatLee <natlee.work@gmail.com>
2023-05-08 01:35:56 +0800

    [update] add blur & darker

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,40 +1,40 @@
 $(document).ready(function () {
-    $.getJSON("assets/data/site.json", function (data) {
+  $.getJSON("assets/data/site.json", function (data) {
       let main = $("#main");
-  
+
       function createHeader(headerText) {
-        return $('<div class="header"></div>').html(`<h1 style="text-align: center">${headerText}</h1>`);
+          return $('<div class="header"></div>').html(`<h1 style="text-align: center">${headerText}</h1>`);
       }
-  
+
       function createArticle(article) {
-        return $(`
-          <article class="preview">
-            <span class="image">
-              <img src="${article.imgSrc}" alt="" />
-            </span>
-            <a target="_blank" rel="noopener noreferrer" href="${article.link}">
-              <h2>${article.title}</h2>
-              <div class="content">
-                <p>${article.description}</p>
-              </div>
-            </a>
-          </article>
-        `);
+          return $(`
+              <article class="preview">
+                  <span class="image">
+                      <img src="${article.imgSrc}" alt="" />
+                  </span>
+                  <a target="_blank" rel="noopener noreferrer" href="${article.link}">
+                      <h2>${article.title}</h2>
+                      <div class="content">
+                          <p>${article.description}</p>
+                      </div>
+                  </a>
+              </article>
+          `);
       }
-  
+
       data.forEach((section) => {
-        main.append(createHeader(section.header));
-  
-        let inner = $('<div class="inner"></div>');
-        let tiles = $('<section class="tiles"></section>');
-        inner.append(tiles);
-  
-        section.articles.forEach((article) => {
-          tiles.append(createArticle(article));
-        });
-  
-        main.append(inner);
-        main.append('<br>'); // Add a line break between sections
+          main.append(createHeader(section.header));
+
+          let inner = $('<div class="inner"></div>');
+          let tiles = $('<section class="tiles"></section>');
+          inner.append(tiles);
+
+          section.articles.forEach((article) => {
+              tiles.append(createArticle(article));
+          });
+
+          main.append(inner);
+          main.append('<br>'); // Add a line break between sections
       });
-    });
   });
+});
